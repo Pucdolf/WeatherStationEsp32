b5ff84bfa319f6758c96b275b82963a1763eac26
Pucdolf <jakub.jh@interia.pl>
2026-04-03 21:00:17 +0200

    feat: add login page and update navigation logic for external links

--- js/script.js
+++ js/script.js
@@ -33,25 +33,30 @@ const allLinks = document.querySelectorAll("a:link");
 console.log(allLinks);
 allLinks.forEach(function (link) {
    link.addEventListener("click", function (e) {
-      e.preventDefault();
       const href = link.getAttribute("href");
 
-      //Scroll back to top
-      if (href == "#")
-         window.scrollTo({
-            top: 0,
-            behavior: "smooth",
-         });
+      // Only prevent default behavior if the link is an internal anchor (#)
+      if (href && href.startsWith("#")) {
+         e.preventDefault();
 
-      //Scroll to other links
-      if (href != "#" && href.startsWith("#")) {
-         const sectionEl = document.querySelector(href);
-         sectionEl.scrollIntoView({ behavior: "smooth" });
-      }
+         //Scroll back to top
+         if (href === "#") {
+            window.scrollTo({
+               top: 0,
+               behavior: "smooth",
+            });
+         }
 
-      //Close mobile navigation
-      if (link.classList.contains("main-nav-link")) {
-         headerEl.classList.toggle("nav-open");
+         //Scroll to other links
+         if (href !== "#" && href.startsWith("#")) {
+            const sectionEl = document.querySelector(href);
+            if (sectionEl) sectionEl.scrollIntoView({ behavior: "smooth" });
+         }
+
+         //Close mobile navigation
+         if (link.classList.contains("main-nav-link")) {
+            headerEl.classList.toggle("nav-open");
+         }
       }
    });
 });
