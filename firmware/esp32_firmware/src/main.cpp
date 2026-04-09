7b99ad64325e72f6d2cc17adca5f3b37d47ae69c
Pucdolf <jakub.jh@interia.pl>
2026-04-09 21:51:16 +0200

    feat: add DS18B20 temperature sensor support and update monitoring logic

--- firmware/esp32_firmware/src/main.cpp
+++ firmware/esp32_firmware/src/main.cpp
@@ -1,14 +1,23 @@
 #include <Arduino.h>
 #include <DHT.h>
+#include<OneWire.h>
+#include<DallasTemperature.h>
 
+//DHT11
 #define DHTPIN 15
 #define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
 
+//DS18B20
+#define ONE_WIRE_BUS 4
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature ds18b20(&oneWire);
+
+//LEDS
 const int ledRed = 25;
 const int ledYellow = 26;
 const int ledGreen = 27;
 
-DHT dht(DHTPIN, DHTTYPE);
 
 // put function declarations here:
 int myFunction(int, int);
@@ -18,9 +27,10 @@ void setup() {
   // int result = myFunction(2, 3);
 
   Serial.begin(115200);
-  Serial.println("Start");
+  Serial.println("Weather Station is starting...");
 
   dht.begin();
+  ds18b20.begin();
 
   pinMode(ledRed, OUTPUT);
   pinMode(ledYellow, OUTPUT);
@@ -31,28 +41,42 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
-  float h = dht.readHumidity();
-  float t = dht.readTemperature();
+  float hDHT = dht.readHumidity();
+  float tDHT = dht.readTemperature();
 
+  ds18b20.requestTemperatures();
+  float tDS = ds18b20.getTempCByIndex(0);
+
+  Serial.print("\n-----------DHT11------------\n");
   Serial.print("Wilgotnosc: ");
-  Serial.print(h);
+  Serial.print(hDHT);
   Serial.print(" %\tTemperatura: ");
-  Serial.print(t);
+  Serial.print(tDHT);
   Serial.println(" *C");
 
-  if(isnan(h) || isnan(t)){
+  Serial.print("\n----------DS18B20----------\n");
+  Serial.print("Temperatura: ");
+  Serial.print(tDS);
+  Serial.println(" *C");
 
-  digitalWrite(ledRed, HIGH);
-  delay(2000);
-  digitalWrite(ledRed,LOW);
-  digitalWrite(ledYellow, HIGH);
-  delay(2000);
-  digitalWrite(ledYellow,LOW);
-  digitalWrite(ledGreen, HIGH);
-  delay(2000);
-  digitalWrite(ledGreen,LOW);
+
+  if(isnan(hDHT) || isnan(tDHT) || tDS <= -100.0){
+    digitalWrite(ledRed, HIGH);
+    digitalWrite(ledYellow,LOW);
+    digitalWrite(ledGreen,LOW);
   }
+  else{
 
+    digitalWrite(ledRed, HIGH);
+    delay(2000);
+    digitalWrite(ledRed,LOW);
+    digitalWrite(ledYellow, HIGH);
+    delay(2000);
+    digitalWrite(ledYellow,LOW);
+    digitalWrite(ledGreen, HIGH);
+    delay(2000);
+    digitalWrite(ledGreen,LOW);
+  }
   delay(2000);
 }
 
